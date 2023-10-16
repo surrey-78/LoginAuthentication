ec6ab3e9bdbce3b40c4f948f5f89221a359e11cc
sethu-ravichandran <rsethu1997@gmail.com>
2023-10-16 11:01:54 +0530

    Add. Included LoginComponent module.

--- src/components/LoginComponent/LoginComponent.jsx
+++ src/components/LoginComponent/LoginComponent.jsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+
+
+
 
 const LoginComponent = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const emailHandler = (event) =>{
+        setEmail(event.target.value)
+      }
+    
+    const passwordHandler = (event) =>{
+        setPassword(event.target.value)
+      }
+    
+    const formSubmitHandler = (event) =>{
+        event.preventDefault()
+        console.log(email, password)
+      }
+
+
   return (
-    <div>LoginComponent</div>
+    <React.Fragment>
+        <form onSubmit={formSubmitHandler}>
+        <div className='mb-3'>
+            <label>Email</label>
+            <input
+                type='email'
+                className='form-control'
+                placeholder='Enter your email address'
+                value={email}
+                onChange={emailHandler}
+                required
+            />
+        </div>
+
+        <div className='mb-3'>
+            <label>Password</label>
+            <input
+                type='password'
+                className='form-control'
+                placeholder='Enter your password'
+                value={password}
+                onChange={passwordHandler}
+                required
+            />
+        </div>
+
+        <div className='mb-3'>
+          <div className='custom-checkbox custom-control'>
+            <input 
+              type='checkbox'
+              className='custom-control-input'
+              id='customCheck1'
+              />
+
+            <label className='custom-control-label' htmlFor='customCheck1'>
+              Remember me?
+            </label>
+          </div>
+          <div className='d-grid'>
+            <button type='submit' className='btn btn-primary' >Submit</button>
+          </div>
+          <p className='forgot-password text-right'>
+            Forgot <a href='#'>Password?</a>
+          </p>
+
+          <p className='text-right'>
+            New User? <Link to='/signup'>Register here!</Link>
+          </p>
+
+        </div>
+
+      </form>
+    </React.Fragment>
   )
 }
 
